e8dfe5a71ed4e8510dd543e3a5cac6e40cb3afb0
Letticia Nicoli <letticia.nicoli@lambda3.com.br>
2018-04-24 17:45:09 -0300

    Init project

---
+++ demoAppCenter/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace demoAppCenter
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
