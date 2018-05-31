3f9ee2473b22d7592482167c72250bbcb5b11bd9
Letticia Nicoli <letticia.nicoli@lambda3.com.br>
2018-05-31 17:28:13 -0300

    Remove ItemsViewModel

--- demoAppCenter/ViewModels/ItemsViewModel.cs
+++ demoAppCenter/ViewModels/ItemsViewModel.cs
@@ -1,55 +1,56 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Threading.Tasks;
+﻿//Remove for demonstration - App Center
+//using System;
+//using System.Collections.ObjectModel;
+//using System.Diagnostics;
+//using System.Threading.Tasks;
 
-using Xamarin.Forms;
+//using Xamarin.Forms;
 
-namespace demoAppCenter
-{
-    public class ItemsViewModel : BaseViewModel
-    {
-        public ObservableCollection<Item> Items { get; set; }
-        public Command LoadItemsCommand { get; set; }
+//namespace demoAppCenter
+//{
+//    public class ItemsViewModel : BaseViewModel
+//    {
+//        public ObservableCollection<Item> Items { get; set; }
+//        public Command LoadItemsCommand { get; set; }
 
-        public ItemsViewModel()
-        {
-            Title = "Check-in";
-            Items = new ObservableCollection<Item>();
-            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+//        public ItemsViewModel()
+//        {
+//            Title = "Check-in";
+//            Items = new ObservableCollection<Item>();
+//            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
-            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
-            {
-                var _item = item as Item;
-                Items.Add(_item);
-                await DataStore.AddItemAsync(_item);
-            });
-        }
+//            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+//            {
+//                var _item = item as Item;
+//                Items.Add(_item);
+//                await DataStore.AddItemAsync(_item);
+//            });
+//        }
 
-        async Task ExecuteLoadItemsCommand()
-        {
-            if (IsBusy)
-                return;
+//        async Task ExecuteLoadItemsCommand()
+//        {
+//            if (IsBusy)
+//                return;
 
-            IsBusy = true;
+//            IsBusy = true;
 
-            try
-            {
-                Items.Clear();
-                var items = await DataStore.GetItemsAsync(true);
-                foreach (var item in items)
-                {
-                    Items.Add(item);
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-            finally
-            {
-                IsBusy = false;
-            }
-        }
-    }
-}
+//            try
+//            {
+//                Items.Clear();
+//                var items = await DataStore.GetItemsAsync(true);
+//                foreach (var item in items)
+//                {
+//                    Items.Add(item);
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Debug.WriteLine(ex);
+//            }
+//            finally
+//            {
+//                IsBusy = false;
+//            }
+//        }
+//    }
+//}
