3afec311099d206ac85eefae647d714f6fc6bd07
Letticia Nicoli <letticia.nicoli@lambda3.com.br>
2018-07-19 12:15:30 -0300

    Refactoring demo

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -15,8 +15,6 @@ namespace demoAppCenter.iOS
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
-
-
             return base.FinishedLaunching(app, options);
         }
     }
